7ae18826d31690f4926db9aead9d5f489cfe000a
abdouaziz <abdouaziz@gmail.com>
2022-05-10 10:54:11 +0000

    fixed features

--- MixSpeech/features.py
+++ MixSpeech/features.py
@@ -1,13 +1,14 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 
 class FeaturesEncoder (nn.Module):
-    def __init__(self,):
+    def __init__(self,input_size):
         super(FeaturesEncoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv1d(in_channels=1, out_channels=512, kernel_size=10, stride=5),
+            nn.Conv1d(in_channels=input_size, out_channels=512, kernel_size=10, stride=5),
             nn.Conv1d(in_channels=512, out_channels=512, kernel_size=3, stride=2),
             nn.Conv1d(in_channels=512, out_channels=512, kernel_size=3, stride=2),
             nn.Conv1d(in_channels=512, out_channels=512, kernel_size=3, stride=2),
@@ -20,10 +21,25 @@ class FeaturesEncoder (nn.Module):
     def forward(self, x):
         for layer in self.encoder:
             x = layer(x)
-            #print("My output",x.size())
             layer_norm = nn.LayerNorm(x.size()[1:])
-            x = layer_norm(x) #https://pytorch.org/docs/stable/generated/torch.nn.LayerNorm.html, look at the example
-            print(torch.mean(x, dim=[1,2])) #results are of the order of e-9, so it worked well
-            x = self.gelu(x) #https://pytorch.org/docs/stable/generated/torch.nn.GELU.html?highlight=gelu#torch.nn.GELU 
+            x = layer_norm(x)
+            x = self.gelu(x) 
         return x
+
+
+
+
+
+
+if __name__ == '__main__':
+    encoder = FeaturesEncoder()
+    x = torch.randn(4, 1, 1000)
+    y = encoder(x)
+    print(y.shape)
+
+
+
+
+
+
 
