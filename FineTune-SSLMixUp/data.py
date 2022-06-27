9e4eede4fdae35e791e1841d208f2bbfef0a6742
abdouaziz <abdouaziz@gmail.com>
2022-06-27 22:17:51 +0000

    path to data

---
+++ FineTune-SSLMixUp/data.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import torch
+
+
+
+
+def get_data(data_path):
+    """
+    Loads the data from the given path.
+    """
+    data = pd.read_csv(data_path)
+    return data
+
+
+    
+with open("/Users/aziiz/Documents/Works/NLP/mixspeech/text") as f:
+    data = f.read().split("\n")
+
+
+
+file_name=[]
+sentences=[]
+
+for i in data:
+    file_name.append(str(i[:18]))
+    sentences.append(i[18:])
+
+audio_data = {
+    'ID':file_name,
+    'transcription':sentences
+}
+
+
+wolof_train = pd.DataFrame(audio_data)
+wolof_train = wolof_train[:12000]
+wolof_train.head()
+
+ 
