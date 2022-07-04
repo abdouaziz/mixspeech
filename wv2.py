2ccde3ea97616f72a05d2c5cf112cd995e84fe14
abdouaziz <abdouaziz@gmail.com>
2022-07-04 09:42:48 +0000

    configuration

--- wv2.py
+++ wv2.py
@@ -3,4 +3,3 @@ from transformers import  Wav2Vec2Config , Wav2Vec2ForCTC
 # Initializing a Wav2Vec2 facebook/wav2vec2-base-960h style configuration
 configuration = Wav2Vec2Config()
 configuration.num_layers = 2
-configuration.hidden_size = 512
