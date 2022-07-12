e57de901b19871a30248f5f173cc173952c57919
abdouaziz <abdouaziz@gmail.com>
2022-07-12 23:01:04 +0000

    clean code

--- wv2.py
+++ wv2.py
@@ -1,29 +1,46 @@
-from unicodedata import name
+from torch.nn import Module
+import torch.nn as nn 
+from typing import Dict, List, Tuple , Optional
+from torch import Tensor
 
 
-class Personn :
-    def __init__(self , name , age , gender):
-        self.__name=name
-        self.__age =age
-        self.__gender=gender
+class FeatureExtractor(Module):
+    """Extract features from audio
+    Args:
+        conv_layers (nn.ModuleList):
+            convolution layers
+    """
 
+    def __init__(
+        self,
+        conv_layers: nn.ModuleList,
+    ):
+        super().__init__()
+        self.conv_layers = conv_layers
 
-    @property
-    def Name(self):
-        return self.__name
+    def forward(
+        self,
+        x: Tensor,
+        length: Optional[Tensor],
+    ) -> Tuple[Tensor, Optional[Tensor]]:
+        """
+        Args:
+            x (Tensor):
+                Input Tensor representing a batch of audio,
+                shape: ``[batch, time]``.
+            length (Tensor or None, optional):
+                Valid length of each input sample. shape: ``[batch, ]``.
+        Returns:
+            Tensor:
+                The resulting feature, shape: ``[batch, frame, feature]``
+            Optional[Tensor]:
+                Valid length of each output sample. shape: ``[batch, ]``.
+        """
+        if x.ndim != 2:
+            raise ValueError("Expected the input Tensor to be 2D (batch, time), " "but received {list(x.shape)}")
 
-    @Name.setter
-    def Name(self, value):
-        self.__name=name
-
-    def mymethod(self):
-        return "je suis la boy"
-
-
-
-if __name__=="__main__":
-    p = Personn("abdou",32,"M")
-    print(p.Name)
-    p.Name="diop"
-    print(p.mymethod)
-    print(p.mymethod())
+        x = x.unsqueeze(1)  # (batch, channel==1, frame)
+        for layer in self.conv_layers:
+            x, length = layer(x, length)  # (batch, feature, frame)
+        x = x.transpose(1, 2)  # (batch, frame, feature)
+        return x, length
